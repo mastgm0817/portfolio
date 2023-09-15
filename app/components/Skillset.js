3353e65df591ccf0c4ed144ced98a352ea330966
mastgm0817 <mastgm0817@gmail.com>
2023-09-16 02:09:29 +0900

    skill set finished

--- app/components/Skillset.js
+++ app/components/Skillset.js
@@ -22,29 +22,96 @@ const Skillset = () => {
             <div className="sk-section-content">
               <div className="sk-section-content-item">
                 <div className="sk-content-item-title">Frontend</div>
-                <div className="sk-content-item-image">tech image</div>
+                <div className="sk-content-item-image">
+                  <Image
+                    src="/images/skillset/Frontend.png"
+                    alt="My Image"
+                    width={300}
+                    height={450}
+                  />
+                </div>
               </div>
               <div className="sk-section-content-item">
-                <div className="sk-content-item-title">Backend</div>
-                <div className="sk-content-item-image">tech image</div>
+                <div
+                  className="sk-content-item-title"
+                  style={{ color: "blue" }}
+                >
+                  Backend
+                </div>
+                <div className="sk-content-item-image">
+                  <Image
+                    src="/images/skillset/Backend.png"
+                    alt="My Image"
+                    width={300}
+                    height={450}
+                  />
+                </div>
               </div>
               <div className="sk-section-content-item">
-                <div className="sk-content-item-title">Deployment</div>
-                <div className="sk-content-item-image">tech image</div>
+                <div
+                  className="sk-content-item-title"
+                  style={{ color: "purple" }}
+                >
+                  Deployment
+                </div>
+                <div className="sk-content-item-image">
+                  <Image
+                    src="/images/skillset/Deployment.png"
+                    alt="My Image"
+                    width={300}
+                    height={450}
+                  />
+                </div>
               </div>
             </div>
             <div className="sk-section-content">
               <div className="sk-section-content-item">
-                <div className="sk-content-item-title">Version Control</div>
-                <div className="sk-content-item-image">tech image</div>
+                <div
+                  className="sk-content-item-title "
+                  style={{ color: "black" }}
+                >
+                  Version Control
+                </div>
+                <div className="sk-content-item-image">
+                  <Image
+                    src="/images/skillset/VCS.png"
+                    alt="My Image"
+                    width={300}
+                    height={200}
+                  />
+                </div>
               </div>
               <div className="sk-section-content-item">
-                <div className="sk-content-item-title">Communication</div>
-                <div className="sk-content-item-image">tech image</div>
+                <div
+                  className="sk-content-item-title"
+                  style={{ color: "skyblue" }}
+                >
+                  Communication
+                </div>
+                <div className="sk-content-item-image">
+                  <Image
+                    src="/images/skillset/Communication.png"
+                    alt="My Image"
+                    width={300}
+                    height={200}
+                  />
+                </div>
               </div>
               <div className="sk-section-content-item">
-                <div className="sk-content-item-title">Certificate</div>
-                <div className="sk-content-item-image">tech image</div>
+                <div
+                  className="sk-content-item-title"
+                  style={{ color: "goldenrod" }}
+                >
+                  Certificate
+                </div>
+                <div className="sk-content-item-image">
+                  <Image
+                    src="/images/skillset/Certificate.png"
+                    alt="My Image"
+                    width={300}
+                    height={350}
+                  />
+                </div>
               </div>
             </div>
           </div>
