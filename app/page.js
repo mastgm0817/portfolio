26d0f8f16879c601cb4eaeb1b1782d0e488bbdb4
mastgm0817 <mastgm0817@gmail.com>
2023-09-21 06:16:12 +0900

    nav 위치

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
   useEffect(() => {
     // 스크롤 이벤트 리스너를 추가합니다.
     function handleScroll() {
-      if (window.scrollY > 150) {
+      if (window.scrollY > 60) {
         // 스크롤 위치가 200 이상인 경우
         setIsNavFixed(true);
         setIsVisible(true);
